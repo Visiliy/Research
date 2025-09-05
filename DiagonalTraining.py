86ccdf649770932676dd54da5a7e61e3fa239b6a
Vasiliy <vasiliy@116.1.168.192.fryazino.net>
2025-09-05 09:21:07 +0300

    /

--- DiagonalTraining.py
+++ DiagonalTraining.py
@@ -11,8 +11,9 @@ class DiagonalTraining(nn.Module):
         self.w_array2 = nn.ModuleList()
 
         for i in range(self.seq_len):
-            self.w_array1.append(nn.Linear(i+1, i+1))
-            self.w_array2.append(nn.Linear(i+1, i+1))
+            diagonal_length = i + 1
+            self.w_array1.append(nn.Linear(diagonal_length, diagonal_length))
+            self.w_array2.append(nn.Linear(diagonal_length, diagonal_length))
 
         del self.w_array2[-1]
 
@@ -22,24 +23,29 @@ class DiagonalTraining(nn.Module):
             array_w = []
             l = i
             r = 0
-            while r < i:
+            while r <= i and l >= 0:
                 array_w.append(x[:, r, l])
                 r += 1
                 l -= 1
-            array_w = torch.tensor(array_w)
-            array_w= self.w_array1[i](array_w)
-            array_w = array_w.tolist()[::-1]
-
+            
+            if len(array_w) == 0:
+                continue
+                
+            
+            array_w = torch.stack(array_w, dim=1)
+            array_w = self.w_array1[i](array_w)
             l = i
             r = 0
-            while r < i:
-                x[:, r, l] = array_w[r]
+            idx = 0
+            while r <= i and l >= 0 and idx < array_w.shape[1]:
+                x[:, r, l] = array_w[:, idx]
                 r += 1
                 l -= 1
+                idx += 1
         return x
 
 
 if __name__ == "__main__":
     model = DiagonalTraining(seq_len=10)
-    x = torch.randn((1, 10, 10))
+    x = torch.randn((8, 10, 10))
     print(model(x).shape)
